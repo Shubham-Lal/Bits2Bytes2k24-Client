d55457b90e75151642b32fd04f6cdacdec829322
Jay9874 <jayprakashsharma225@gmail.com>
2024-07-22 23:58:31 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route index='' />
       </Routes>
