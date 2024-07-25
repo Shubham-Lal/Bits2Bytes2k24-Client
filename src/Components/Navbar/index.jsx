104e01f5c3ee8cf741db913014da2ab29bff1f3c
Shubham-Lal <im.shubhamlal@gmail.com>
2024-07-25 11:37:02 +0530

    preload images

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -13,22 +13,18 @@ const navlinks = [
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <div className='navbar-container'>
-        <div className='navbar-logo'>
-          <span>
-            <img src='/images/logo.png' alt='bits2bytes-logo' />
-          </span>
-          <span>Bits2Bytes 2k24</span>
-        </div>
-        <div className='navbar-links'>
-          <ul className='navbar-links-container'>
-            {navlinks.map((link, index) => (
-              <li key={index}>
-                <NavLink to={link.to}>{link.tag}</NavLink>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className='navbar-logo'>
+        <img src='/images/logo.png' alt='bits2bytes-logo' />
+        <span>Bits2Bytes 2k24</span>
+      </div>
+      <div className='navbar-links'>
+        <ul className='navbar-links-container'>
+          {navlinks.map((link, index) => (
+            <li key={index}>
+              <NavLink to={link.to}>{link.tag}</NavLink>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
